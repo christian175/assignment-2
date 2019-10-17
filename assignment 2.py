99070f04c16b6f8c6010887cda38b231f8ce36e7
christian175 <55259741+christian175@users.noreply.github.com>
2019-10-17 14:07:12 -0400

    Update assignment 2.py

--- assignment 2.py
+++ assignment 2.py
@@ -6,14 +6,10 @@ for x in range (0,10) :
 
 keepsearching = True
 while keepsearching:
-    search = input('enter search or "End" to stop: ')
+    search = input('search for name or type "End" to stop: ')
     if search in names:
         print("name found")
     elif search == "End":
         keepsearching = False
     else:
         print("name not found")
-    
-
-
-    
